00c26044bb84f4441dae851358892caa0f1ae65b
yoharnu <dani91@gmail.com>
2025-06-11 23:28:01 +0000

    Refactor AlertsController to streamline API calls by removing unnecessary API fetching logic and directly returning incident, construction, and weather data.

--- KnoxTrafficCenter/Controllers/AlertsController.cs
+++ KnoxTrafficCenter/Controllers/AlertsController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using KnoxTrafficCenter.Services;
 using System.Threading.Tasks;
+using KnoxTrafficCenter.Models.TDOT;
+using System.Collections.Generic;
 
 namespace KnoxTrafficCenter.Controllers
 {
@@ -11,22 +13,22 @@ namespace KnoxTrafficCenter.Controllers
         [HttpGet("incidents")]
         public async Task<IActionResult> GetIncidents()
         {
-            var api = await tdotApiService.GetTDOTAPIAsync();
-            return Ok(api.Incidents);
+            var incidents = await tdotApiService.GetIncidentsAsync();
+            return Ok(incidents ?? new List<Event>());
         }
 
         [HttpGet("construction")]
         public async Task<IActionResult> GetConstruction()
         {
-            var api = await tdotApiService.GetTDOTAPIAsync();
-            return Ok(api.Construction);
+            var construction = await tdotApiService.GetConstructionAsync();
+            return Ok(construction ?? new List<Event>());
         }
 
         [HttpGet("weather")]
         public async Task<IActionResult> GetWeather()
         {
-            var api = await tdotApiService.GetTDOTAPIAsync();
-            return Ok(api.Weather);
+            var weather = await tdotApiService.GetWeatherAsync();
+            return Ok(weather ?? new List<Event>());
         }
     }
 }
